68ed6b6f9b0743e13ec619a1add89056af11e088
Jennifer Techel <jennifertechel@hotmail.com>
2023-01-13 12:30:05 +0100

    codebase

---
+++ src/sketch.ts
@@ -0,0 +1,50 @@
+//---- GLOBAL VARIABLES ----//
+// let game: Game;
+// let sound: p5.SoundFile
+
+/**
+ * Built in preload function in P5
+ * This is a good place to load assets such as
+ * sound files, images etc...
+ */
+function preload() {
+    // sound: p5.SoundFile = loadSound('../assets/mySound.wav');
+}
+
+/**
+ * Built in setup function in P5
+ * This is a good place to create your first class object
+ * and save it as a global variable so it can be used
+ * in the draw function below
+ */
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    frameRate(60);
+    // noCursor();
+    
+    // game = new Game();
+}
+
+/**
+ * Built in draw function in P5
+ * This is a good place to call public methods of the object
+ * you created in the setup function above
+ */
+function draw() {
+    background('blue');
+    fill('green');
+    stroke('white');
+    strokeWeight(10);
+    circle(width * .5, height * .5, width * 0.2);
+
+    // game.update();
+    // game.draw();
+}
+
+
+/**
+ *  Built in windowResize listener function in P5
+ */
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
