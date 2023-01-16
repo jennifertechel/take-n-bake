9dc1d8657c278793a1f9dae44a2f9d6828581108
Tara Skoglund <skoglundtara@gmail.com>
2023-01-16 14:14:07 +0100

    created arreys for each recipes

--- src/recipe.ts
+++ src/recipe.ts
@@ -1,3 +1,13 @@
 class Recipe {
     
 }
+
+let allRecipes: string[] = ["Pancake", "Blueberry pie", "Chocolate cake"];
+
+let pancakeRecipe: string[] = ["eggs", "butter", "milk"];
+let blueberryPieRecipe: string[] = ["eggs", "butter", "blueberries", "flour", "sugger"];
+let chocolateCakeRecipe: string[] = ["eggs", "butter", "milk", "flouer", "sugger", "chocolate", "strawberries"];
+
+let amountIngredients: number[] = [1,2,3,4,5,6,7,8,9];
+
+console.log(allRecipes, pancakeRecipe, blueberryPieRecipe, chocolateCakeRecipe)
