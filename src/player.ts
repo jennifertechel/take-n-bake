fc227d92b14126f2b102fdbbfe8b3755f9d8b7cb
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-01-16 12:25:17 +0100

    Add player class with handleInput method

--- src/player.ts
+++ src/player.ts
@@ -1,3 +1,16 @@
-class Player {
+class Player extends movingObject {
+    constructor (image: p5.Image, position: p5.Vector, size: p5.Vector, velocity: p5.Vector) {
+      super(image, position, size, velocity);
+  }
 
+  public handleInput() {
+      // handle arrow key input to update the player's velocity
+      if (keyIsDown(LEFT_ARROW)) {
+          this.velocity.x = -5;
+      } else if (keyIsDown(RIGHT_ARROW)) {
+          this.velocity.x = 5;
+      } else {
+          this.velocity.x = 0;
+      }
+    }
 }
