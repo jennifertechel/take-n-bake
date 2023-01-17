7ae669226180b5682f26dd6a9dc3b5c699735547
Tara Skoglund <skoglundtara@gmail.com>
2023-01-17 11:35:16 +0100

    conected allRecipes to ingredient arrys

--- src/recipe.ts
+++ src/recipe.ts
@@ -1,13 +1,23 @@
 class Recipe {
-    
+    let allRecipes: (string[])[] = [];
+    let pancakeRecipe: string[] = ["eggs", "butter", "milk"];
+    let blueberryPieRecipe: string[] = ["eggs", "butter", "blueberries", "flour", "suger"];
+    let chocolateCakeRecipe: string[] = ["eggs", "butter", "milk", "flour", "suger", "chocolate", "strawberries"];
+
+    allRecipes.push(pancakeRecipe);
+    allRecipes.push(blueberryPieRecipe);
+    allRecipes.push(chocolateCakeRecipe);
+
+    console.log(allRecipes);   
+
+
 }
 
-let allRecipes: string[] = ["Pancake", "Blueberry pie", "Chocolate cake"];
+// let allRecipes: string[] = ["Pancake", "Blueberry pie", "Chocolate cake"];
 
-let pancakeRecipe: string[] = ["eggs", "butter", "milk"];
-let blueberryPieRecipe: string[] = ["eggs", "butter", "blueberries", "flour", "sugger"];
-let chocolateCakeRecipe: string[] = ["eggs", "butter", "milk", "flouer", "sugger", "chocolate", "strawberries"];
+// let pancakeRecipe: string[] = ["eggs", "butter", "milk"];
+// let blueberryPieRecipe: string[] = ["eggs", "butter", "blueberries", "flour", "sugger"];
+// let chocolateCakeRecipe: string[] = ["eggs", "butter", "milk", "flouer", "sugger", "chocolate", "strawberries"];
 
-let amountIngredients: number[] = [1,2,3,4,5,6,7,8,9];
+let amountIngredients: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-console.log(allRecipes, pancakeRecipe, blueberryPieRecipe, chocolateCakeRecipe)
