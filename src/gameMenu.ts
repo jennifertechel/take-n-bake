7f66e4565a14371c57a67062877deefeaf63534d
HaNNa89 <petersson.h@hotmail.com>
2023-01-17 14:54:36 +0100

    added background page to gameMenu class

--- src/gameMenu.ts
+++ src/gameMenu.ts
@@ -1,17 +1,24 @@
 class GameMenu {
+    protected position: p5.Vector;
+    protected image: p5.Image;
+    
+    
+    constructor (image: p5.Image) {
+        this.position = createVector();
+        this.image = loadImage("assets/images/frostingSmal.svg");
+
+    }
+
     public setup() {
 
     }
     public draw() {
-
+        image(this.image,0, 0);
     }
     public update() {
 
     }
 
-    constructor () {
-        
-    }
 
 
     
