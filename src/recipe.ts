c1092914f68aa9c6df37693c62c1f9a8e21e50ce
Tara Skoglund <skoglundtara@gmail.com>
2023-01-18 21:50:21 +0100

    change ingredient amount in draw so it will render to text

--- src/recipe.ts
+++ src/recipe.ts
@@ -12,19 +12,38 @@ class Recipe {
 
     }
 
-    private pancakeRecipe: string[];
-    private blueberryPieRecipe: string[];
-    private chocolateCakeRecipe: string[];
+    private pancakeRecipe: string[] = ["eggs", "butter", "milk"];
+    private pancakeAmount: number[] = [2, 1, 3];
 
-    private amountIngredients: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    private blueberryPieRecipe: string[] = ["eggs", "butter", "blueberries", "flour", "sugar"];
+    private blueberryPieAmount: number[] = [1, 2, 1, 3, 6];
 
-    public draw(){
-    
+    private chocolateCakeRecipe: string[] = ["eggs", "butter", "milk", "flour", "sugar", "chocolate", "strawberries"];
+    private chocolateCakeAmount: number[] = [3, 1, 5, 3, 2, 3, 4]
+
+    public update() {
+        
     }
 
+    public draw() {
+        for (let i = 0; i < this.allRecipes.length; i++) {
+            let recipe = this.allRecipes[i];
+            let amount;
+            if (i === 0) {
+                amount = this.pancakeAmount;
+            } else if (i === 1) {
+                amount = this.blueberryPieAmount;
+            } else {
+                amount = this.chocolateCakeAmount;
+            }
+            text("Recipe" + (i + 1) + ":");
+            for (let j = 0; j < recipe.length; j++) {
+                text(recipe[j] + ":" + amount[j]);
+            }
+        }
+    }
+    
 }
 
+
 let recipe = new Recipe();
-console.log(recipe);
-
-
