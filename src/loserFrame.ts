cdc09b3eb26057e13a5bc1031b8a8c6f336435f9
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-01-18 15:16:12 +0100

    create loserFrame that exteds from MessageBox

--- src/loserFrame.ts
+++ src/loserFrame.ts
@@ -1,95 +1,30 @@
-let buttonHover= 200;
-let button2Hover= 0;
+class LoserFrame extends MessageBox {
+    private text: string;
+    private button1: Button;
+    private button2: Button;
+    private image: any;
 
-class LoserFrame {
-    protected position: p5.Vector;
-    protected size: p5.Vector;
-    protected borderColor: String;
-    protected hoverColor: String;
-    protected messageBackgroundColor: String;
-    protected image: p5.Image;
-    protected textMessage: String;
-    protected backgroundImg: p5.Image;
-
-    constructor (
-        image: p5.Image)
-     {
-        this.position = createVector(width/2 - 525, height/2 - 305.5);
-        this.size = createVector(1050, 611);
-        this.borderColor = "C6E3DE";
-        this.hoverColor = "23c471"
-        this.messageBackgroundColor = "F5F5F5";
+    constructor(title: string, text: string, button1: Button, button2: Button, image: any) {
+        super(title);
+        this.text = text;
+        this.button1 = button1;
+        this.button2 = button2;
         this.image = image;
-        this.textMessage = "Ops...try again!";
-        this.backgroundImg = loadImage("assets/images/Logo.svg");
     }
 
     public update() {
-        
+        super.update();
+        this.button1.update();
+        this.button2.update();
     }
-    
+
     public draw() {
-        push();
-        let borderColor = color("#"+ this.borderColor);
-        let bgColorMessage = color("#"+ this.messageBackgroundColor);
-        fill(bgColorMessage)
-        stroke(borderColor);
-        strokeWeight(60);
-        rect(this.position.x, this.position.y, this.size.x, this.size.y);
-        image(this.image, width/2-200, height/2 - 130, 400, 300);
-        
-        textSize(64);
-        fill('black');
-        noStroke();
-        textStyle(BOLD);
-        text(this.textMessage, width/2 - 256, height/2 - 195, this.size.x, this.size.y);
-
-        let button = createButton('Menu');;
-        let button2 = createButton('Restart');
-        button.position(width/2 - 182,  height/2 + 150);
-        button2.position(width/2 + 10, height/2 + 150);
-        button.style('background-color', borderColor)
-        button2.style('background-color', borderColor)
-        button.size(160, 70)
-        button2.size(160, 70)
-        button.style('border', 0)
-        button2.style('border', 0)
-        button.style('font-size', 22 + 'px')
-        button2.style('font-size', 22 + 'px')
-        button.style('color', "#fff")
-        button2.style('color', "#fff")
-
-        let hoverCol = color("#C6E3DE");
-
-        // create a hover button; 
-        button.mouseOver(() => {
-            button.style('color', "#000")
-            console.log("over")
-        });
-
-        button.mouseOut(() => {
-            button.style('background', "#C6E3DE")
-            console.log("out")
-        });
-
-        button2.mouseOver(() => {
-            button2.style('background-color', "#C6E3DE")
-            console.log("over")
-        });
-
-        button2.mouseOut(() => {
-            button2.style('background-color', borderColor.toString())
-            console.log("out")
-        });
-        pop();
+        super.draw();
+        fill("#000");
+        textSize(32);
+        text(this.text, innerWidth/2, innerHeight/2);
+        this.button1.draw();
+        this.button2.draw();
+        image(this.image, innerWidth/2 - 150, innerHeight/2 + 100);
     }
 }
-
-// function changeBG() {
-//   let val = random(255);
-//   background(val);
-// }
-
-// class button {
-//     button.mousePressed(changeBG);
-// }
