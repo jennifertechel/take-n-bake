d425c519d884e238db4543f9c8400f1a351fa666
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-01-19 14:42:06 +0100

    changed to display winnerScene

--- src/game.ts
+++ src/game.ts
@@ -19,9 +19,9 @@ class Game implements IScene {
       this.menuScene = new MenuScene(this);
       this.recipeScene = new RecipeScene();
       this.levelScene = new LevelScene();
-      this.winnerScene = new WinnerScene();
+      this.winnerScene = new WinnerScene(this);
       this.looserScene = new LooserScene(this);
-      this.activeScene = "startScene";
+      this.activeScene = "winnerScene";
       this.time = 0;
     }
 
@@ -49,6 +49,7 @@ class Game implements IScene {
 
     public draw() {
       this.menuScene.draw();
+      this.winnerScene.draw();
     }
 
     public setActiveScene(scene: Scene) {
