f7eae2a8637cacba1ab8dc9021e3af43842d58e0
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-01-19 22:46:18 +0100

    added box for recipe and tableCloth img

--- src/game.ts
+++ src/game.ts
@@ -18,7 +18,7 @@ class Game implements IScene {
       this.startScene = new StartScene();
       this.menuScene = new MenuScene(this);
       this.recipeScene = new RecipeScene();
-      this.levelScene = new LevelScene();
+      this.levelScene = new LevelScene(this);
       this.winnerScene = new WinnerScene(this);
       this.looserScene = new LooserScene(this);
       this.activeScene = "winnerScene";
@@ -28,6 +28,8 @@ class Game implements IScene {
     }
 
     public update() {
+
+        // player.update();
 
       this.time += deltaTime;
       if (this.time > 1000) {
@@ -53,8 +55,12 @@ class Game implements IScene {
 
     public draw() {
       this.menuScene.draw();
-      this.winnerScene.draw();
-      this.looserScene.draw();
+    //   this.winnerScene.draw();
+    //   this.looserScene.draw();
+      this.levelScene.draw();
+
+      player.handleInput();
+      player.draw();
     }
 
     public setActiveScene(scene: Scene) {
--- src/levelScene.ts
+++ src/levelScene.ts
@@ -1,3 +1,35 @@
-class LevelScene {
-  
+/// <reference path="player.ts" />
+
+class LevelScene extends Player {
+    private tableCloth: p5.Image;
+    private recipe: string;
+    private recipeBackground: p5.Vector;
+    private game: IScene;
+
+    constructor(game: IScene) {
+        super(images.playerBowl, createVector(width * 0.5, height * .75), createVector(220, 220), createVector(0, 0));
+        this.tableCloth = images.backgroundObjects.tableCloth;
+        this.recipe = "tillfälligt /n 4 av något annat"
+        this.recipeBackground = createVector((innerWidth/4-225), 580, 50);
+
+        this.game = game;
+    }
+
+    public update() {
+        // this.game.setActiveScene("play")
+    }
+    
+    public draw() {
+       this.recipeBackground
+
+       // Recipe
+       strokeWeight(20);
+       stroke("#C6E3DE")
+       fill("#F5F5F5");
+       rect(30, 30, 250, 382);
+       image(this.tableCloth, 0, innerHeight-180, innerWidth, 180);
+
+           // Move this and change player image 
+        // player = new Player(images.playerBowl, createVector(width * 0.5, height * .75), createVector(220, 220), createVector(0, 0));
+   }
 }
