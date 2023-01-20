6d2f6ff165e72afa4cebf0052c65c2b211fac524
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-01-20 11:06:39 +0100

    Co-authored-by: Martina Kreivi <mkreivi@users.noreply.github.com> Co-authored-by: Jennifer Techel <jennifertechel@users.noreply.github.com> Co-authored-by: Hanna Petersson <hanna.petersson@medieinstitutet.se> Co-authored-by: David Jensen <Tsourdox@users.noreply.github.com>

---
+++ src/recipeFactory.ts
@@ -0,0 +1,57 @@
+type Ingredient = "apple" | "";
+
+interface IIngredientData {
+  name: Ingredient;
+  amount: number;
+}
+
+interface IRecipeData {
+  name: string;
+  ingredients: IIngredientData[];
+  image: p5.Image;
+}
+
+class RecipeFactory {
+  private recipes: IRecipeData[];
+
+  constructor() {
+    this.recipes = [{
+      name: "Pancakes",
+      image: images.recipes.pancake,
+      ingredients: [{
+        name: '',
+        amount: 2
+      }, {
+        name: '',
+        amount: 2
+      }, {
+        name: '',
+        amount: 2
+      }]
+    }, {
+      name: "Blueberry Pie",
+      image: images.recipes.pie,
+      ingredients: [{
+        name: '',
+        amount: 2
+      }, {
+        name: '',
+        amount: 2
+      }, {
+        name: '',
+        amount: 2
+      }]
+    }]
+  }
+
+  public getRecipe(level: number) {
+    const recipeData = this.recipes[level - 1];
+    return new Recipe(recipeData)
+  }
+
+  public getIngredient(name: Ingredient) {
+    if (name === "apple") {
+      return new Ingredient(name, images.ingredients.apple);
+    }
+  }
+}
