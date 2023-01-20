14d5ba85511a5cb1a84a10ba0ed565ae48d481d2
Martina Kreivi <martinakreivi@gmail.com>
2023-01-20 11:56:21 +0100

    Logo shows on webb

--- src/startScene.ts
+++ src/startScene.ts
@@ -2,6 +2,7 @@ class StartScene {
 
     private image: p5.Image;
     private image2: p5.Image;
+    private logo: p5.Image;
     private buttonGoToGame: Button;
     private game: IScene;
 
@@ -12,6 +13,7 @@ class StartScene {
         this.buttonGoToGame = new Button(createVector(600, 600), "Start game");
         this.image = images.backgroundObjects.frostingBig;
         this.image2 = images.backgroundObjects.sprinkles;
+        this.logo = images.logo;
     }
 
     public update() {
@@ -24,6 +26,7 @@ class StartScene {
         this.buttonGoToGame.draw();
         image(this.image, 0, 0);
         image(this.image2, width - this.image2.width, height - this.image2.height ); // position x y, bild x y
+        image(this.logo, 300, 300); // position x y, bild x y
     }
 
 
