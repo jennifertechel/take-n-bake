afc061017f722ca975d31d293eebc675b0b0c90e
HaNNa89 <petersson.h@hotmail.com>
2023-01-20 11:10:58 +0100

    change buttons in menueScene

--- src/menuScene.ts
+++ src/menuScene.ts
@@ -1,16 +1,21 @@
 class MenuScene {
   private game: Game;
+  private buttonStartGame: Button;
+  private buttonQuitGame: Button;
 
-  constructor(game: Game) {
-    this.game = game;
+  constructor(game: IScene) {
+  
+    this.buttonStartGame = new Button (createVector (250, 250), "Start");
+    this.buttonQuitGame = new Button (createVector (200, 200), "Quit");
   }
 
   public update() {
-    // this.game.update(); // förhindra
+    
   }
 
   public draw() {
-    // this.game.draw();
+    this.buttonStartGame.draw();
+    this.buttonQuitGame.draw();
   }
     
 }
