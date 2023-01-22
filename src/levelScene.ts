454d2fc8d9f2dec116160bd8432e3de3388f317a
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-01-22 13:32:03 +0100

    change line height of recipe text

--- src/levelScene.ts
+++ src/levelScene.ts
@@ -23,19 +23,18 @@ class LevelScene extends Player {
     }
     
     public draw() {
-       this.recipeBackground
-       noCursor();
-       // Recipe background
+        noCursor();
+
+        // Recipe background
+        this.recipeBackground
        strokeWeight(20);
        stroke("#C6E3DE")
        fill("#F5F5F5");
        rect(30, 30, 220, 362);
 
-       let recipeLines = this.recipe.split("\n"); // Needs to change when we import recipe
-       let yPos = 125; // start position for first line of text
-
-       
        // Recipe text
+       let recipeLines = this.recipe.split("\n");
+       let yPos = 125; // start position for first line of text
        textSize(26);
        fill("#A74272");
        textAlign(LEFT, TOP);
@@ -45,16 +44,16 @@ class LevelScene extends Player {
          text(recipeLines[i], 60, yPos);
          yPos += 40; // increment the y-position for next line of text
        }
-    //    text(this.recipe, 60, 125);
+
+       // Recipe title
        textStyle(BOLD);
        text(this.recipeTitle, 60, 70);
        textSize(38);
+
        // Timer
        text(this.timer, windowWidth -120, 40);
+
        // Tablecloth
        image(this.tableCloth, 0, innerHeight-180, innerWidth, 180);
-
-        // Move this and change player image 
-        // player = new Player(images.playerBowl, createVector(width * 0.5, height * .75), createVector(220, 220), createVector(0, 0));
    }
 }
