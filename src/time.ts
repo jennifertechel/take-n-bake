66305cf0909e3b62a534263183c8c820afee07a1
Martina Kreivi <martinakreivi@gmail.com>
2023-01-24 09:01:39 +0100

    Co-authored-by: petersson.h@hotmail.com <petersson.h@hotmail.com>

--- src/time.ts
+++ src/time.ts
@@ -1,30 +1,3 @@
-
-
 class Time {
-
-    private timer: number;
-    private lastTime: number;
     
-    constructor() {
-        this.timer = 0;
-        this.lastTime = millis();
-    }
-
-    update() {
-        if (millis() - this.lastTime > 1000){
-            this.timer++; 
-            this.lastTime = millis();
-        }
-
-    }
-
-    draw() {
-        
-        text(this.time, 20, 400 );
-    }
-
-    time () {
-        
-    }
-     
 }
