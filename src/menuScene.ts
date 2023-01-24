7fbadad7096dd79484d2d6ef5b73ce1ad56c94d6
HaNNa89 <petersson.h@hotmail.com>
2023-01-24 16:37:50 +0100

    added the final text to menuScene, and change the logo position.

--- src/menuScene.ts
+++ src/menuScene.ts
@@ -16,7 +16,8 @@ class MenuScene {
     this.logoImage = images.logo;
     this.sprinklesImage = images.backgroundObjects.sprinkles;
     this.gameRulesHeading = "Welcome!";
-    this.gameRules = "Grandma is in a jam. \nShe doesn’t have the ingredients to bake for her grandkids. \nHelp her catch the right ingredients \nto complete the recipe.";
+    this.gameRules = `Grandma is in a jam.\nShe doesn’t have the ingredients to bake for her grandkids.\nHelp her catch the right ingredients to complete the recipe.
+    But make sure you don’t ruin the cake by \ncatching the wrong ingredient or you will have to start all over.`;
     this.buttonStartGame = new Button (createVector(innerWidth/2-225, 580), "Start", "recipeScene");
     this.buttonQuitGame = new Button (createVector(innerWidth/2+25, 580), "Quit", "startScene");
     //första värdet vågrätt, andra värdet lodrätt
@@ -28,7 +29,7 @@ class MenuScene {
 
   public draw() {
     image(this.smallFrostingImage, 0, 0, innerWidth, this.smallFrostingImage.height);
-    image(this.logoImage, 50, 20, 180, 180);
+    image(this.logoImage, 50, 0, width/7, height/5);
     image(this.sprinklesImage, width - this.sprinklesImage.width, height - this.sprinklesImage.height );
     fill("#808080");
     textAlign(CENTER);
@@ -37,7 +38,7 @@ class MenuScene {
     text(this.gameRulesHeading, 970, 300);
     textSize(30);
     textAlign(CENTER);
-    text(this.gameRules, 970, 430);
+    text(this.gameRules, 970, 440);
     
     
     this.buttonStartGame.draw();
