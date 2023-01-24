bbbf5be3f3694d154e2bd641327ce17f2cebe6eb
HaNNa89 <petersson.h@hotmail.com>
2023-01-24 16:43:54 +0100

    added jdocs

--- src/menuScene.ts
+++ src/menuScene.ts
@@ -1,5 +1,8 @@
 //import p5, { Image } from "p5";
 
+/**
+ * This is the Menuscen class. 
+ */
 class MenuScene {
   private game: IScene;
   private smallFrostingImage: p5.Image;
@@ -26,7 +29,9 @@ class MenuScene {
     this.buttonStartGame.update();
     this.buttonQuitGame.update();
   }
-
+  /**
+   * Draw the images, buttons and menue-text to this menuescen. 
+   */
   public draw() {
     image(this.smallFrostingImage, 0, 0, innerWidth, this.smallFrostingImage.height);
     image(this.logoImage, 50, 0, width/7, height/5);
@@ -39,8 +44,6 @@ class MenuScene {
     textSize(30);
     textAlign(CENTER);
     text(this.gameRules, 970, 440);
-    
-    
     this.buttonStartGame.draw();
     this.buttonQuitGame.draw();
     this.buttonStartGame.checkHover();
