225056b8b7015e930775f6bed300ca28caee1c11
isabellaisaxsson <117035529+isabellaisaxsson@users.noreply.github.com>
2023-01-27 16:18:16 +0100

    Centered recipes tried to make space between recipes

--- src/recipeScene.ts
+++ src/recipeScene.ts
@@ -24,14 +24,18 @@ class RecipeScene extends MessageBox {
         textSize(60);
         fill("#000000");
         textAlign(LEFT);
+        // textLeading(100);
         noStroke();
         let recipe = this.recipeFactory.getRecipe(1);
         let yPos = 430; // start position for first line of text
-        
+        let xPos = 1000;
     
         for(let i = 0; i < recipe.getIngredients().length; i++) {
-            text(recipe.getIngredients()[i].amount + " " + recipe.getIngredients()[i].name, 60, yPos+i*40);
+            text(recipe.getIngredients()[i].amount + " " + recipe.getIngredients()[i].name, 850, yPos+i*40);
+           
         }
+        
+        
 
         textStyle(BOLD);
         fill("#808080");
