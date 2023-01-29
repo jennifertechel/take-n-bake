6623c6ff5ea7c5595238abae2340b103f83e3982
isabellaisaxsson <isabella.isaksson2@outlook.com>
2023-01-29 16:05:20 +0100

    Made page for laptops

--- src/recipeScene.ts
+++ src/recipeScene.ts
@@ -13,7 +13,7 @@ class RecipeScene extends MessageBox {
         super(" "); //Messagebox title
     this.RecipeSceneBackground = createVector((innerWidth/4-225), 580, 50); //Scene size
     this.tableCloth = images.backgroundObjects.tableCloth; 
-    this.buttonStartPlaying = new Button(createVector(innerWidth/2-110, 650), "Start", "startScene"); //Creates button to start game
+    this.buttonStartPlaying = new Button(createVector(innerWidth/2-110, 550), "Start", "startScene"); //Creates button to start game
     this.game = game;
     this.recipeFactory = new RecipeFactory();
     }
@@ -24,28 +24,25 @@ class RecipeScene extends MessageBox {
         textSize(60);
         fill("#000000");
         textAlign(LEFT);
-        // textLeading(100);
         noStroke();
         let recipe = this.recipeFactory.getRecipe(1);
-        let yPos = 430; // start position for first line of text
-        let xPos = 1000;
+        let yPos = 325; // start position for first line of text
+        
     
         for(let i = 0; i < recipe.getIngredients().length; i++) {
-            text(recipe.getIngredients()[i].amount + " " + recipe.getIngredients()[i].name, 850, yPos+i*40);
-           
+            text(recipe.getIngredients()[i].amount + " " + recipe.getIngredients()[i].name, 665, yPos+i*40);
         }
-        
-        
 
         textStyle(BOLD);
         fill("#808080");
-        text(recipe.getName(), 820, 250);
+        text(recipe.getName(), 635, 200);
         textSize(100);
         textAlign(CENTER, TOP);
+        textLeading(10);
 
         this.RecipeSceneBackground
 
-        image(this.tableCloth, 0, innerHeight-120, innerWidth, 180); //imports tablecloth
+        image(this.tableCloth, 0, innerHeight-60, innerWidth, 180); //imports tablecloth
         this.buttonStartPlaying.draw();
         this.buttonStartPlaying.checkHover();
     }
