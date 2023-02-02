bf80c211f06ab5d3d103bec7632a2fb05e3e84d3
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-02-02 11:09:29 +0100

    fix margins between stars

--- src/winnerScene.ts
+++ src/winnerScene.ts
@@ -31,27 +31,27 @@ class WinnerScene extends MessageBox {
         // Draw the messageBox with title
         super.draw();
 
-        // Add all stars
-        for (let i = 0; i < this.getStars(); i++) {
-            push();
-            let x = innerWidth/2 - 124 + (i * 95);
-            let y = innerHeight/2 + 55;
-            x = x / 0.45;
-            y = y / 0.45;
-            scale(0.45);
-            image(this.starFilled, x, y);
-            pop();
-        }
-        for (let i = 0; i < 3 - Number(this.getStars()); i++) {
-            push();
-            let x = innerWidth/2 - 50 + (i * 80) + Number(this.getStars()) * 50;
-            let y = innerHeight/2 + 55;
-            x = x / 0.45;
-            y = y / 0.45;
-            scale(0.45);
-            image(this.starOutlined, x, y);
-            pop();
-        }  
+    // Add all stars
+    for (let i = 0; i < this.getStars(); i++) {
+        push();
+        let x = innerWidth/2 - 128 + (i * 95);
+        let y = innerHeight/2 + 55;
+        x = x / 0.45;
+        y = y / 0.45;
+        scale(0.45);
+        image(this.starFilled, x, y);
+        pop();
+    }
+    for (let i = 0; i < 3 - Number(this.getStars()); i++) {
+        push();
+        let x = innerWidth/2 - 128 + ((i + Number(this.getStars())) * 95);
+        let y = innerHeight/2 + 55;
+        x = x / 0.45;
+        y = y / 0.45;
+        scale(0.45);
+        image(this.starOutlined, x, y);
+        pop();
+    }
 
         this.buttonNextLevel.draw();
         this.buttonMenu.draw();
