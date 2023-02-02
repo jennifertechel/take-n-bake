eb7bfe73d5b92ff0abccc580da021bde8bff893d
Martina Kreivi <martinakreivi@gmail.com>
2023-02-02 14:05:10 +0100

    menuScene done

--- src/menuScene.ts
+++ src/menuScene.ts
@@ -21,8 +21,8 @@ class MenuScene {
     this.gameRulesHeading = "Welcome!";
     this.gameRules = `Grandma is in a jam.\nShe doesn’t have the ingredients to bake for her grandkids.\nHelp her catch the right ingredients to complete the recipe.
     But make sure you don’t ruin the cake by \ncatching the wrong ingredient or you will have to start all over.`;
-    this.buttonStartGame = new Button (createVector(innerWidth/2-225, 550), "Start", "recipeScene");
-    this.buttonQuitGame = new Button (createVector(innerWidth/2+25, 550), "Quit", "startScene");
+    this.buttonStartGame = new Button (createVector(innerWidth/2-225, 650), "Start", "recipeScene");
+    this.buttonQuitGame = new Button (createVector(innerWidth/2+25, 650), "Quit", "startScene");
     
   }
   public update() {
@@ -40,7 +40,7 @@ class MenuScene {
     textAlign(CENTER);
     textFont("Josefin Sans");
     textSize(40);
-    text(this.gameRulesHeading, innerWidth/2-485, innerHeight/2-240, 970, innerWidth/2-485);
+    text(this.gameRulesHeading, innerWidth/2-485, innerHeight/2-240, 970, 230);
     textSize(30);
     textAlign(CENTER);
     text(this.gameRules, innerWidth/2, innerHeight/2+20);
