913f3fca8edb3c3faae8ac37f9d08c99236a6f2c
Tara Skoglund <skoglundtara@gmail.com>
2023-02-02 10:18:47 +0100

    clean up js.doc

--- src/recipe.ts
+++ src/recipe.ts
@@ -4,11 +4,6 @@ class Recipe {
     private ingredients: IIngredientData[];
     private name: string;
 
-    /**
-   * Creates an instance of Recipe.
-   * @constructor
-   * @param {IRecipeData} recipe - The recipe data.
-   */
   constructor(recipe: IRecipeData) {
     this.ingredients = recipe.ingredients;
     this.name = recipe.name;
